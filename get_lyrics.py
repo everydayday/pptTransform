cbcb039cbb8da4e86cb4885a77cb69cc37a9e596
everydayday <kdh39278@gmail.com>
2022-01-11 15:46:41 +0900

    pptx 폰트,크기 기능구현

--- get_lyrics.py
+++ get_lyrics.py
@@ -5,11 +5,24 @@
 
 str = []
 def get_lyrics():
-    while(True):
-        line = input()
-        if(line == 'quit'):
-            break
-        str.append(line)
+    str.append('this')
+    str.append('is')
+    str.append('code')
+    str.append('can')
+    str.append('u')
+    str.append('guess')
+    str.append('what')
+    str.append('it')
+    str.append('is?')
+    str.append('안녕하세요')
+    str.append('반갑습니다')
+    str.append('안녕안녕')
+    str.append('인사해야지?')
+
+    # while(True):
+    #     line = input()
+    #     if(line == 'quit'):
+    #         break
+    #     str.append(line)
     return str
 
-print(str)
--- slidemain.py
+++ slidemain.py
@@ -1,16 +1,17 @@
 from pptx import Presentation  # 라이브러리
-from ProjectFile.get_lyrics import get_lyrics
-from math import ceil # ppt 갯수 올림
+from pptx.util import Pt
+from get_lyrics import get_lyrics
+from math import ceil # slide 갯수구하기 by 올림
 
 # 전체 가사 입력
 str = get_lyrics()
 slide_num = ceil(len(str)/2)   # slide 갯수 (올림)
 
 
-slide_str = [] # slide마다 입력되는 2줄의 가사
+slide_str = [] # index마다 입력되는 2줄의 가사 존재
 
+# slide_str 만들기
 for i in range(slide_num):
-    print(i)
     try:
         slide_str.append(str[i*2] + '\n' + str[i*2+1])
     except IndexError: # 가사줄의 갯수가 홀수여서 str[i*2]까지만 해당할 시
@@ -29,8 +30,10 @@ for i in range(0, slide_num):
     content = slide.placeholders[0]
     content.text = slide_str[i]
     title = prs.slides[i].shapes.title
-    title.text_frame.paragraphs[i].font.name = '함초름돋음'
-    #title.text_frame.paragraphs[i].font.size.Pt(30)
+    title.text_frame.paragraphs[0].font.name = '함초름돋음'
+    title.text_frame.paragraphs[1].font.name = '함초름돋음'
+    title.text_frame.paragraphs[0].font.size = Pt(30)
+    title.text_frame.paragraphs[1].font.size = Pt(30)
 
 
 
